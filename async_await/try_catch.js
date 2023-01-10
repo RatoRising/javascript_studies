a8ba88b14cbc32cb74d1a80eeb63f9ad2f368e83
RatoRising <reinaneves@hotmail.com>
2023-01-10 20:05:58 -0300

    update try_catch example

--- async_await/try_catch.js
+++ async_await/try_catch.js
@@ -1,3 +1,4 @@
+// This try_catch will return the success as there is no error
 try {
 	console.log('Start of try runs'); // (1) <--
 	console.log('End of try (never reached)'); // (2)
@@ -5,6 +6,7 @@ try {
 	console.log(`Error has occurred!`); // (3) <--
 }
 
+// This try_catch will catch the error as the varialble lalala is not defined
 try {
 	console.log('Start of try runs'); // (1) <--
 
@@ -15,3 +17,16 @@ try {
 	console.log(`Error has occurred!`); // (3) <--
 }
 
+// This is maybe clearer as you can see the conditional working and validating as true or false
+const number = 51;
+try {
+   if (number > 50) {
+      console.log('Success');
+   } else {
+      throw new Error('The number is lower');
+   }
+   console.log('Hellow number is ok');
+} catch (error) {
+   console.log('An error was caught!');
+   console.log('Error message: ' + error);
+}
